a1a7eac9e51780482ca45e28f38e0a28e661e91e
Abhinavxox <abhinavxox@gmail.com>
2023-03-22 20:48:50 +0530

    <added optional error handlers>:[]

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -32,6 +32,24 @@ module.exports = (err, req, res, next) => {
       error = new ErrorHandler(message, 400);
     }
 
+    //Handling mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Handling wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+      const message = "Json Web Token is invalid. Try Again!";
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Handling expired JWT error
+    if (err.name === "TokenExpiredError") {
+      const message = "Json Web Token is expired";
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",
