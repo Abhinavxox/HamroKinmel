5257be78c04046d68389b1a6428b7ebaf9206ef8
Abhinavxox <abhinavxox@gmail.com>
2023-03-24 15:27:48 +0530

    <>:[]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import Header from "./components/layout/Header";
+import Home from "./components/layout/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div className="App">hello world!</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import StoreLogo from "../../images/store.png";
+import UserAvatar from "../../images/user.png";
+import CartIcon from "../../images/cart.png";
+
+const Header = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Header;
